5df4ab6467acff9f3ee085033fc3514aa67c357f
Jaeeunykim <jaeeun.kje@gmail.com>
2017-06-10 12:43:04 +0900

    add echo_j code

---
+++ ch5/echo_j.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	int i;
+
+	for(i=1; i<argc; i++){
+		printf("%s", argv[i]);
+
+		if(i< argc -1)
+			printf(" ");
+		else
+			printf("");
+	}
+	printf("\n");
+	return 0;
+}
